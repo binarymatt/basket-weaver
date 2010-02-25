4436d253541f0a901fe77b7576828a1abf523415
Whit Morriss <wmorriss@3441702.local>
2010-02-25 10:28:33 -0600

    add pushd, pm decorator

--- basketweaver/utils.py
+++ basketweaver/utils.py
@@ -1,6 +1,8 @@
+from contextlib import contextmanager
 from webob import Response
 import functools
 import inspect
+import os
 
 
 def unwsgify(func, catch_exc_info=True):
@@ -25,3 +27,41 @@ def unwsgify(func, catch_exc_info=True):
         status = spec[3][1][0]
         return Response(body=ret[0], status=status)
     return wrapper
+
+
+@contextmanager
+def pushd(dir):
+    '''
+    modified from http://www.blueskyonmars.com/projects/paver .
+    
+    A context manager (Python 2.5+ only) for stepping into a 
+    directory and automatically coming back to the previous one. 
+    The original directory is returned. Usage is like this::
+    
+        from __future__ import with_statement
+        # the above line is only needed for Python 2.5
+        
+        from paver.easy import *
+        
+        @task
+        def my_task():
+            with pushd('new/directory') as old_dir:
+                ...do stuff...
+    '''
+    old_dir = os.getcwd()
+    os.chdir(dir)
+    try:
+        yield old_dir
+    finally:
+        os.chdir(old_dir)
+
+
+def pm(func):
+    @functools.wraps(func)
+    def postm(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception, e:
+            import pdb, sys; pdb.post_mortem(sys.exc_info[2])
+            raise e
+    return postm
